5df827b6869145da37398f560dc067c50421466d
zxlordxz <152212726+vmell0@users.noreply.github.com>
2023-11-28 03:09:54 -0300

    1.0.0.6

---
+++ script/CheckUser/checkuserMenu.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python3 /root/CheckUser/checkuserMenu.py
---
+++ script/CheckUser/install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+vermelho="\e[31m"
+verde="\e[32m"
+amarelo="\e[33m"
+azul="\e[34m"
+roxo="\e[38;2;128;0;128m"
+reset="\e[0m"
+
+rm -rf /root/CheckUser/
+rm -f /usr/local/bin/CheckUser
+pkill -9 -f "/root/CheckUser/checkuser.py"
+
+apt update && apt upgrade -y && apt install python3 git -y
+git clone https://github.com/UlekBR/CheckUser.git
+chmod +x /root/CheckUser/checkuserMenu.sh
+ln -s /root/CheckUser/checkuserMenu.sh /usr/local/bin/CheckUser
+
+clear
+echo -e "Para iniciar o menu digite: ${verde}CheckUser${reset}"
