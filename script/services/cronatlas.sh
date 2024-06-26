328b0d9e162be148136cc20c21a5817472c34bcd
vmell0 <152212726+vmell0@users.noreply.github.com>
2024-06-26 05:32:04 -0300

    2.3.2.7

--- script/services/cronatlas.sh
+++ script/services/cronatlas.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-curl -silent https://ssh.cvpn.top/onlines.php >/dev/null 2>&1 && curl -silent https://ssh.cvpn.top/checkpag.php >/dev/null 2>&1
+curl -silent https://gestor.cvpn.top/onlines.php >/dev/null 2>&1 && curl -silent https://gestor.cvpn.top/checkpag.php >/dev/null 2>&1
